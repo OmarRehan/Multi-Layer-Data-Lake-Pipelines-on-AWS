cb16813ecaa6fd3900d8c3b6d82aac6176d73bf9
Omar Khaled Rehan <o_khaled201553@yahoo.com>
2020-06-22 21:57:14 +0200

    Dev : - Separated all IL tables into different jobs - Generalized a single function to load all standard lookups on the LZ with dynamic input

---
+++ load_landing_zone/load_landing_zone_standard_lookup.py
@@ -0,0 +1,65 @@
+import logging
+import sys
+
+from helper_functions.initialize_spark_session import initialize_spark_session
+from pyspark.sql.types import StructField, StructType, StringType
+from constants import dict_dbs_locations, edge_node_path
+from sql_queries.landing_zone_ddl import list_landing_zone_standard_lookups
+from helper_functions.zip_csv_to_gzip_parquet import zip_csv_to_gzip_parquet
+from helper_functions.loop_files import loop_files
+import os
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s ")
+
+
+def load_lz_standard_lookup(table_name):
+    landing_zone_location = dict_dbs_locations.get('LANDING_ZONE_LOC')
+
+    # Initializing a Spark session
+    spark = initialize_spark_session('load_lz' + table_name)
+
+    # Loading the standard lookups with the same schema in landing_zone
+    try:
+
+        # Standard schema for the standard lookups
+        schema_lookups_schema = StructType(
+            [
+                StructField("Code", StringType(), True),
+                StructField("Description", StringType(), True)
+            ]
+        )
+
+        df_file = spark \
+            .read \
+            .schema(schema_lookups_schema) \
+            .option("header", "true") \
+            .csv(os.path.join(edge_node_path, table_name, '*.csv'))
+
+        df_file.write.format("csv") \
+            .mode("overwrite") \
+            .option("sep", ",") \
+            .option('header', 'true') \
+            .save(os.path.join(landing_zone_location, table_name))
+
+        logging.info(f'{table_name} has been loaded in the landing zone.')
+
+    except Exception as e:
+        logging.error(f"Failed to load {table_name} in the landing zone,{e}")
+
+
+if __name__ == '__main__':
+
+    table_name = None
+
+    # finding and parsing table_name arg to be provided to the function
+    for arg in sys.argv:
+        if 'table_name' in arg:
+            table_name = arg.split('=')[1]
+
+    if table_name is not None:
+
+        load_lz_standard_lookup(table_name)
+
+    else:
+        raise Exception(
+            'table_name argument is required to start loading, ex: table_name=L_AIRLINE_ID')
